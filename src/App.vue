d4814a7caabd1465cf07e5111c855c0ea1ded529
Adriano Yokoya <adriano_yokoya@hotmail.com>
2020-11-03 13:10:40 -0300

    First Version

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
+  <div class="pagina">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
+.pagina {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
